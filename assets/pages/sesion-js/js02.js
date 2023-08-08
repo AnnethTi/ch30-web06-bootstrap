49c890713e3f2770983f864d2f8eb70797608e28
AnnethTi <annethtinajero@gmail.com>
2023-08-08 12:55:14 -0600

    ci: solucion contar en numeros de letras p

--- assets/pages/sesion-js/js02.js
+++ assets/pages/sesion-js/js02.js
@@ -21,9 +21,9 @@ hoisting: comportamiento en JS que permite a las declaración de variables(var)
 o funciones se eleven al comienzo de su ámbito antes de que se ejecute el código.
 
 */
-console.log( multiplica(4, 3));
+console.log(multiplica(4, 3));
 
-function multiplica(a, b){
+function multiplica(a, b) {
     return a * b;
 }
 
@@ -41,23 +41,23 @@ sintaxis:
     };
 */
 
-const sum = function sumatoria(a, b){
+const sum = function sumatoria(a, b) {
     return a + b;
 }
-console.log(`El resultado de 4 + 2 : ${ sum(4,2) }`);
+console.log(`El resultado de 4 + 2 : ${sum(4, 2)}`);
 
 let saludo; // undefined
 const localidad = "tlaxcala";
 
-if ( localidad == "buenos aires"){
-    saludo = function ( name ) { return `Holis ${name}`};
+if (localidad == "buenos aires") {
+    saludo = function (name) { return `Holis ${name}` };
 }
 else {
-    saludo = function ( name ) { return `Buenos días ${name} `};
+    saludo = function (name) { return `Buenos días ${name} ` };
 }
 
-console.log( saludo("Sergio"));
-console.log( saludo("David"));
+console.log(saludo("Sergio"));
+console.log(saludo("David"));
 
 // las funciones expresadas pueden ser re definidas
 
@@ -75,9 +75,9 @@ sintaxis:
 // son funciones que se ejecutan automáticamente una vez que se definen, 
 // sin necesidad de ser llamadas explícitamente. 
 
-( function( name ){
+(function (name) {
     console.log(`Me llamo ${name} y me autoinvoco`)
-} )('ChatGPT')
+})('ChatGPT')
 
 
 /*
@@ -107,7 +107,7 @@ sintaxis:
 const areaRectangulo = (base, altura) => base * altura;
 
 
-console.log(`El área es: ${ areaRectangulo(3, 9)} m2`);
+console.log(`El área es: ${areaRectangulo(3, 9)} m2`);
 
 /*
  ------------ Parametros por defecto -----------------------
@@ -122,9 +122,9 @@ console.log(`El área es: ${ areaRectangulo(3, 9)} m2`);
  * @param {number} altura del triangulo
  * @returns {number} base * altura / 2
  */
-const areaTriangulo = (base = 12, altura = 5) => (base * altura)/2;
+const areaTriangulo = (base = 12, altura = 5) => (base * altura) / 2;
 
-console.log(`El área es: ${ areaTriangulo()} m2`);
+console.log(`El área es: ${areaTriangulo()} m2`);
 
 
 console.log((1000).toString())
@@ -143,16 +143,107 @@ sintaxis:
 
 */
 
-const sumatoriaMultiplesNumeros = (a = 0,b = 0, ...rest) => {
+const sumatoriaMultiplesNumeros = (a = 0, b = 0, ...rest) => {
     let suma = a + b;
-    for(let i = 0; i < rest.length; i++) suma+= rest[i]
+    for (let i = 0; i < rest.length; i++) suma += rest[i]
     return suma;
 }
 
-console.log(`Sumatoria de multiples numeros: ${sumatoriaMultiplesNumeros(2,5)}`);
-console.log(`Sumatoria de multiples numeros: ${sumatoriaMultiplesNumeros(2,5,10,9)}`);
-console.log(`Sumatoria de multiples numeros: ${sumatoriaMultiplesNumeros(2,5,10,9,20,8)}`);
+console.log(`Sumatoria de multiples numeros: ${sumatoriaMultiplesNumeros(2, 5)}`);
+console.log(`Sumatoria de multiples numeros: ${sumatoriaMultiplesNumeros(2, 5, 10, 9)}`);
+console.log(`Sumatoria de multiples numeros: ${sumatoriaMultiplesNumeros(2, 5, 10, 9, 20, 8)}`);
 console.log(`Sumatoria de multiples numeros: ${sumatoriaMultiplesNumeros(2)}`);
 
 
-/* Funciones de Callback */
+/* Funciones de Callback 
+Es una funcion que se pasa a otra funcion como argumento
+Se para en el argumento como referencia (sin parentesis)
+
+
+Funciones que se invocan en otro tiempo
+*/
+
+
+function saludoALosPokemones() {
+    return 'yo te elijo';
+}
+
+function eligiendoPokebola(saludo, nombre) {
+    console.log('Hola, estas en la liga pokemon');
+    console.log('Elige a tu mejor pokemon');
+    console.log(saludo(nombre));
+}
+
+eligiendoPokebola(saludoALosPokemones);
+
+eligiendoPokebola(nombre => `quiii, soy ${nombre}`, 'Charizard')
+
+/*
+  Ejercicio 4
+  Crear un programa que itere sobre dos arreglos;
+  si hay cursos en común, imprimirlos en la consola.
+
+ student1Courses = ["Math", "English", "Programming", "Biology", "Physics", "Music"];
+student2Courses = ["Geography", "Spanish", "Programming", "Music"];
+
+  salida: "Cursos en común: Programming, Music"
+*/
+const student1Courses = ["Math", "English", "Programming", "Biology", "Physics", "Music"];
+const student2Courses = ["Geography", "Spanish", "Programming", "Music"];
+
+function cursosEnComun(student1Courses, student2Courses) {
+    const commonCourses = []; // guardar los cursos en comun
+    for (let i = 0; i < student1Courses.length; i++) { // ["Math", "English", "Programming", "Biology", "Physics", "Music"];
+        for (let j = 0; j < student2Courses.length; j++) { // ["Geography", "Spanish", "Programming", "Music"]
+            console.log(`student1Courses[i] === student2Courses[j] : ${student1Courses[i] === student2Courses[j]}`)
+            if (student1Courses[i] === student2Courses[j]) {
+                commonCourses.push(student1Courses[i]);
+            }
+        }
+    }
+    return `Cursos en común ${commonCourses}`
+}
+
+console.log(cursosEnComun(student1Courses, student2Courses))
+
+
+//--------------- Resolviendo con filter e include ----------------------
+
+function getCommonCoursesWithFilter(array1Courses, array2Courses) {
+    return array1Courses.filter(course => array2Courses.includes(course));
+}
+console.log(`Común: ${getCommonCoursesWithFilter(student1Courses, student2Courses)}`)
+
+//--------------- Resolviendo con filter e include por partes ----------------------
+console.log("###################################");
+function includeCourse(course, index, array) {
+    console.log(`Elemento ${course}, indice ${index}, include ${student2Courses.includes(course)}`);
+    return student2Courses.includes(course); // evaluación // ["Geography", "Spanish", "Programming", "Music"]
+}
+
+function getCoursesWithFilter(array1Courses) {
+    const commonCourses = array1Courses.filter(includeCourse); // ["Math", "English", "Programming", "Biology", "Physics", "Music"];
+    return commonCourses;
+}
+console.log(`Comúnxpartes: ${getCoursesWithFilter(student1Courses, student2Courses)}`);
+// -------------- Contar la cantidad de caracteres de una frase ---------------------
+/*
+   "peso pluma pica papas con un pico y una pala con un pico pica papas peso pluma";
+
+   Mostrar la cantidad de letras 'p'.
+   Usar funciones flecha.
+*/
+
+//.split('a').length - 1
+
+const cadena = 'Peso Pluma pica papas con un pico y una pala con un pico pica papas peso pluma'
+
+function contarCaracteres(cadena, caracter){
+    return cadena.toLowerCase().split(caracter.toLowerCase()).length -1 
+}
+
+//const contarCaracteres2 = (cadena, caracter) => cadena.split(caracter).length -1 
+const contarCaracteres2 = (cadena, caracter) => cadena.toLowerCase().split(caracter.toLowerCase()).length -1 
+
+console.log(contarCaracteres(cadena, 'P'))
+console.log(contarCaracteres2(cadena, 'P'))
